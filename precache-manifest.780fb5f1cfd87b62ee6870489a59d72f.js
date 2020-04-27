335b4ffbc6b81a22b737d2966e6f518da2a4c2f3
Talia Harrison-Marcassa <hm.talia@gmail.com>
2020-04-27 11:34:18 -0400

    Updates

---
+++ precache-manifest.780fb5f1cfd87b62ee6870489a59d72f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b9cdb7c09bc4c345922b5e69e6c6ea",
+    "url": "/taliahm/testtesttesttest/index.html"
+  },
+  {
+    "revision": "662b08727cb2fd87fd9d",
+    "url": "/taliahm/testtesttesttest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6331edf84075791ffcc",
+    "url": "/taliahm/testtesttesttest/static/js/2.ffd770a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taliahm/testtesttesttest/static/js/2.ffd770a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662b08727cb2fd87fd9d",
+    "url": "/taliahm/testtesttesttest/static/js/main.49a41d3a.chunk.js"
+  },
+  {
+    "revision": "d5da008a1d721a120ea8",
+    "url": "/taliahm/testtesttesttest/static/js/runtime-main.0bb65139.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taliahm/testtesttesttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
